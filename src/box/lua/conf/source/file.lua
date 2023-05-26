e133c4119d20dbde3073ef0794d129b16adf51e4
Alexander Turenko <alexander.turenko@tarantool.org>
2023-05-27 02:12:46 +0300

    conf: support reading a FIFO file as a config

--- src/box/lua/conf/source/file.lua
+++ src/box/lua/conf/source/file.lua
@@ -1,22 +1,70 @@
+local ffi = require('ffi')
+local buffer = require('buffer')
 local fio = require('fio')
 local yaml = require('yaml')
 
 local values = {}
 
+-- Read the file block by block till EOF.
+local function stream_read(fh)
+    local block_size = 1024
+    local buf = buffer.ibuf(block_size)
+    while true do
+        local rc, err = fh:read(buf:reserve(block_size), block_size)
+        if err ~= nil then
+            buf:recycle()
+            return nil, err
+        end
+        if rc == 0 then
+            -- EOF.
+            break
+        end
+        buf:alloc(rc)
+    end
+    local res = ffi.string(buf.rpos, buf:size())
+    buf:recycle()
+    return res
+end
+
+-- Read all the file content disregarding whether its size is
+-- known from the stat(2) file information.
+local function universal_read(file_name, file_kind)
+    local fh, err = fio.open(file_name)
+    if fh == nil then
+        error(('Unable to open %s %q: %s'):format(file_kind, file_name, err))
+    end
+
+    -- A FIFO file has zero size in the stat(2) file information.
+    --
+    -- However, it is useful for manual testing from a shell:
+    --
+    -- $ tarantool --name <...> --config <(echo '{<...>}')
+    --
+    -- Let's read such a file block by block till EOF.
+    local data
+    local err
+    if fio.path.is_file(file_name) then
+        data, err = fh:read()
+    else
+        data, err = stream_read(fh)
+    end
+
+    fh:close()
+
+    if err ~= nil then
+        error(('Unable to read %s %q: %s'):format(file_kind, file_name, err))
+    end
+
+    return data
+end
+
 local function sync(ctx, _iconfig)
     assert(ctx.config_file ~= nil)
 
-    local fh, err = fio.open(ctx.config_file)
-    if fh == nil then
-        error(('Unable to open a config file %q: %s'):format(
-            ctx.config_file, err))
-    end
-    local data = fh:read()
-    fh:close()
-
+    local data = universal_read(ctx.config_file, 'config file')
     local ok, res = pcall(yaml.decode, data)
     if not ok then
-        error(('Unable to parse a config file %q as YAML: %s'):format(
+        error(('Unable to parse config file %q as YAML: %s'):format(
             ctx.config_file, res))
     end
 
