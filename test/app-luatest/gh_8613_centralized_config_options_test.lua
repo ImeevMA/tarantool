40338b4fae6db9e425fe8dc118f36b02bd4cb5b8
Alexander Turenko <alexander.turenko@tarantool.org>
2023-06-19 18:10:42 +0300

    test/config: adjust CLI options test
    The CLI options test is a unit test in fact and doesn't suppose to test
    tarantool startup logic. Let's disable this logic using an override
    module generated during the test.
    
    Starting with `--name <...>` and `--config <...>` options is to be
    tested as part of the test/config-luatest test suite.
    
    NO_DOC=modification of a testing code
    NO_CHANGELOG=see NO_DOC

--- test/app-luatest/gh_8613_centralized_config_options_test.lua
+++ test/app-luatest/gh_8613_centralized_config_options_test.lua
@@ -1,4 +1,5 @@
 local t = require('luatest')
+local treegen = require('test.treegen')
 local justrun = require('test.justrun').tarantool
 
 local g = t.group()
@@ -86,9 +87,52 @@ local cases = {
     },
 }
 
+g.before_all(function(g)
+    treegen.init(g)
+    g.rundir = treegen.prepare_directory(g, {})
+
+    -- Don't try to actually start tarantool instances with
+    -- --name <...> --config <...> options.
+    --
+    -- This is out of scope of the test. Here we just verify
+    -- several successful cases:
+    --
+    -- * Start with an instance name.
+    -- * Start with a script/-e option.
+    -- * Start the interactive REPL.
+    --
+    -- And several failure cases (such as -n option without an
+    -- argument).
+    --
+    -- It is implemented by replacing the function that performs
+    -- the startup with an instance name.
+    --
+    -- Actually this approach cuts out some checks (such as
+    -- presence of a cluster config or presence of the instance
+    -- with the given name in the cluster config), but it is OK
+    -- for a unit alike test.
+    --
+    -- A real tarantool startup would require `--config <...>` or
+    -- `TT_CONFIG_ETCD_*` when `--name <...>` is provided.
+    --
+    -- This logic is to be tested by integration testing as part
+    -- of the test/config-luatest test suite.
+    treegen.write_script(g.rundir, 'override/config.lua', [[
+        return {
+            _startup = function()
+            end
+        }
+    ]])
+end)
+
+g.after_all(function(g)
+    treegen.clean(g)
+    g.rundir = nil
+end)
+
 g.test_tarantool_run = function()
     for _, case in pairs(cases) do
-        local result = justrun('.', case.env, case.args, {nojson = true})
+        local result = justrun(g.rundir, case.env, case.args, {nojson = true})
         t.assert_equals(result.exit_code, 0, 'Plain run is successful')
     end
 end
@@ -98,7 +142,7 @@ g.test_tarantool_run_with_execute = function()
     for _, case in pairs(cases) do
         local runopts = {nojson = true, stderr = expected.stderr}
         local runargs = imerge(case.args, args.execute)
-        local result = justrun('.', case.env, runargs, runopts)
+        local result = justrun(g.rundir, case.env, runargs, runopts)
         t.assert_equals(result.exit_code, 0, 'Execute run is successful')
         t.assert_str_contains(result.stdout,
                               expected.stdout.output, expected.stdout.pattern,
@@ -111,7 +155,7 @@ g.test_tarantool_run_with_interactive = function()
     for _, case in pairs(cases) do
         local runopts = {nojson = true, stderr = expected.stderr}
         local runargs = imerge(case.args, args.interactive)
-        local result = justrun('.', case.env, runargs, runopts)
+        local result = justrun(g.rundir, case.env, runargs, runopts)
         t.assert_equals(result.exit_code, 0, 'Interactive run is successful')
         t.assert_str_contains(result.stdout,
                               expected.stdout.output, expected.stdout.pattern,
@@ -123,14 +167,14 @@ g.test_tarantool_run_with_interactive = function()
 end
 
 g.test_tarantool_cli_name_arg = function()
-    local result = justrun('.', {}, {'-n'}, {nojson = true, stderr = true})
+    local result = justrun(g.rundir, {}, {'-n'}, {nojson = true, stderr = true})
     t.assert_not_equals(result.exit_code, 0, 'Invalid CLI args run is failed')
     t.assert_str_contains(result.stderr, "option requires an argument %-%- 'n'",
                           true, 'Invalid CLI args error')
 end
 
 g.test_tarantool_cli_config_arg = function()
-    local result = justrun('.', {}, {'-c'}, {nojson = true, stderr = true})
+    local result = justrun(g.rundir, {}, {'-c'}, {nojson = true, stderr = true})
     t.assert_not_equals(result.exit_code, 0, 'Invalid CLI args run is failed')
     t.assert_str_contains(result.stderr, "option requires an argument %-%- 'c'",
                           true, 'Invalid CLI args error')
