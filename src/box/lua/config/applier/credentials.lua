7e2074f546286e94176ea9b00f02a0e977ff5925
Mergen Imeev <imeevma@tarantool.org>
2024-02-22 11:07:28 +0300

    config: move sharding role creation to sharding.lua

--- src/box/lua/config/applier/credentials.lua
+++ src/box/lua/config/applier/credentials.lua
@@ -1,4 +1,3 @@
-local expression = require('internal.config.utils.expression')
 local log = require('internal.config.utils.log')
 local digest = require('digest')
 local fiber = require('fiber')
@@ -300,76 +299,6 @@ local function privileges_add_defaults(name, role_or_user, intermediate)
     return res
 end
 
---
--- Create a credential sharding role if it is assigned to any user or role and
--- has not been declared in 'credentials.roles'.
---
--- Replicasets with the sharding storage role must be assigned the credentials
--- sharding role. In these replicasets, the role will be assigned all necessary
--- privileges.
---
--- For replicasets that do not have the sharding storage role assigned, the
--- credentials sharding role will not have any privileges.
---
--- If the snapshot version does not match the Tarantool schema version, DDL
--- operations, including creating roles and granting privileges, are prohibited.
--- For user-created changes, this will throw an error as expected, but since the
--- "sharding" role is created by default, the error may still appear even if the
--- user does not make any changes. To partially avoid this problem, we do not
--- create the "sharding" role if it is not in use. This is a temporary solution.
-local function sharding_role(configdata)
-    local roles = configdata:get('credentials.roles')
-    local users = configdata:get('credentials.users')
-    local has_sharding_role = false
-    for _, role in pairs(roles or {}) do
-        for _, role_role in pairs(role.roles or {}) do
-            has_sharding_role = has_sharding_role or role_role == 'sharding'
-        end
-    end
-    for _, user in pairs(users or {}) do
-        for _, user_role in pairs(user.roles or {}) do
-            has_sharding_role = has_sharding_role or user_role == 'sharding'
-        end
-    end
-    if not has_sharding_role then
-        return
-    end
-
-    -- Add necessary privileges if storage sharding role is enabled.
-    local sharding_roles = configdata:get('sharding.roles')
-    if sharding_roles == nil or #sharding_roles == 0 then
-        return {}
-    end
-    local is_storage = false
-    for _, role in pairs(sharding_roles) do
-        is_storage = is_storage or role == 'storage'
-    end
-    if not is_storage then
-        return {}
-    end
-
-    local funcs = {}
-    --
-    -- The error will be thrown later, in sharding.lua. Here we are simply
-    -- trying to avoid the "module not found" error.
-    --
-    local ok, vshard = pcall(require, 'vshard')
-    if ok and expression.eval('v >= 0.1.25', {v = vshard.consts.VERSION}) then
-        local vexports = require('vshard.storage.exports')
-        local exports = vexports.compile(vexports.log[#vexports.log])
-        for name in pairs(exports.funcs) do
-            table.insert(funcs, name)
-        end
-    end
-    return {
-        privileges = {{
-            permissions = {'execute'},
-            functions = funcs,
-        }},
-        roles = {'replication'},
-    }
-end
-
 local function merge_creds(dest, src)
     assert(dest ~= nil and type(dest) == 'table')
     if src == nil then
@@ -420,10 +349,6 @@ local function get_credentials(config)
     credentials.roles['super'] = credentials.roles['super'] or {}
     credentials.roles['public'] = credentials.roles['public'] or {}
     credentials.roles['replication'] = credentials.roles['replication'] or {}
-
-    -- Add a semi-default role 'sharding'.
-    credentials.roles['sharding'] = credentials.roles['sharding'] or
-                                    sharding_role(config._configdata)
 
     credentials.users = credentials.users or {}
     credentials.users['guest'] = credentials.users['guest'] or {}
