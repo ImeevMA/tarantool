a89e2ee94e5da719f98ea1e28d91b81ac49afc84
Mergen Imeev <imeevma@tarantool.org>
2026-08-25 08:04:00 +0300

    sql: resolve BOOLEAN expressions

--- src/box/sql/ast.c
+++ src/box/sql/ast.c
@@ -628,7 +628,8 @@ expr_from_ast(struct Parse *parser, struct ast_expr *expr)
 	case TK_TRUE:
 	case TK_FALSE:
 	case TK_UNKNOWN:
-		res = expr_leaf(expr, FIELD_TYPE_BOOLEAN);
+		res = sql_expr_new_leaf(expr->op, FIELD_TYPE_BOOLEAN);
+		res->v.b = expr->op == TK_TRUE;
 		break;
 	case TK_VAR_ANON:
 	case TK_VAR_NUM:
