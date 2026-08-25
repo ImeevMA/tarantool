371bfbd96428157c5f35241f08a0a3e2924c689f
Mergen Imeev <imeevma@tarantool.org>
2026-08-25 08:04:00 +0300

    sql: resolve BOOLEAN expression value
    This patch moves the resolution of a boolean expression's value to
    the expression creation stage, rather than to the VDBE opcode
    creation stage.
    
    Part of #13229
    
    NO_DOC=refactoring
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring

--- src/box/sql/sqlInt.h
+++ src/box/sql/sqlInt.h
@@ -1271,6 +1271,12 @@ struct Expr {
 		int iValue;	/* Non-negative integer value if EP_IntValue */
 	} u;
 
+	/** Resolved value of the expression. */
+	union {
+		/** Value for TK_TRUE and TK_FALSE. */
+		bool b;
+	} v;
+
 	/* If the EP_TokenOnly flag is set in the Expr.flags mask, then no
 	 * space is allocated for the fields below this point. An attempt to
 	 * access them will result in a segfault or malfunction.
@@ -2404,6 +2410,10 @@ void sqlClearTempRegCache(Parse *);
  */
 struct Expr *
 sql_expr_new(int op, const struct Token *token);
+
+/** Allocate a new empty expression object with reserved extra memory. */
+struct Expr *
+sql_expr_new_empty(int op, int extra_size);
 
 /**
  * The same as @sa sql_expr_new, but normalizes name, stored in
